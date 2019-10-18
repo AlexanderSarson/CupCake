a76b12c6e3201dca63c47075107867fe09cf5a12
Mads Brandt <mads.brandt93@gmail.com>
2019-10-18 23:01:15 +0200

    Added input validation with js to createUser and login

--- src/main/webapp/js/script.js
+++ src/main/webapp/js/script.js
@@ -1,9 +1,88 @@
 function validateLogin() {
-    var email = document.forms["loginForm"]["email"].value;
+    var email = document.forms["loginForm"]["email"];
+    var errorContainer = document.getElementById("errorMessage");
 
-    if (!(email.includes("@")) || !(email.includes("."))) {
-        alert("Please provide a valid email address")
-        email.focus();
+    // Validates that email includes @ or .
+    if (!(email.value.includes("@")) || !(email.value.includes("."))) {
+        email.classList.add("uk-form-danger");
+        errorContainer.innerHTML = "Please provide a valid email address with a '@' and at least one '.'";
         return false;
     }
+
+    // Submits form
+    document.forms['loginForm'].submit();
+}
+
+function validateAccountCreation() {
+    var name = document.forms["createAccountForm"]["name"];
+    var email = document.forms["createAccountForm"]["email"];
+    var pass = document.forms["createAccountForm"]["password"];
+    var repeatPass = document.forms["createAccountForm"]["repeatPassword"];
+    var errorContainer = document.getElementById("errorMessage");
+
+    name.classList.remove("uk-form-danger");
+    email.classList.remove("uk-form-danger");
+    pass.classList.remove("uk-form-danger");
+    repeatPass.classList.remove("uk-form-danger");
+
+    // Validates that name is not empty
+    if(name.value === "") {
+        name.classList.add("uk-form-danger");
+        errorContainer.innerHTML = "Please fill out the name field";
+        return false;
+    }
+
+    // Validates that name only contains letters and spaces
+    if(!name.value.match(/^[a-zA-Z]+$/)) {
+        name.classList.add("uk-form-danger");
+        errorContainer.innerHTML = "Name can only contain letters and spaces";
+        return false;
+    }
+
+    // Validates that email is not empty
+    if(email.value === "") {
+        email.classList.add("uk-form-danger");
+        errorContainer.innerHTML = "Please fill out the email field";
+        return false;
+    }
+
+    // Validates that email includes @ or .
+    if (!(email.value.includes("@")) || !(email.value.includes("."))) {
+        email.classList.add("uk-form-danger");
+        errorContainer.innerHTML = "Please provide a valid email address with a '@' and at least one '.'";
+        return false;
+    }
+
+    // Validates that password is not empty
+    if(pass.value === "") {
+        pass.classList.add("uk-form-danger");
+        errorContainer.innerHTML = "Please fill out the password field";
+        return false;
+    }
+
+    // Validates that password is at least 8 characters long
+    if (pass.value.length < 8) {
+        pass.classList.add("uk-form-danger");
+        repeatPass.classList.add("uk-form-danger");
+        errorContainer.innerHTML = "Password has to be at least 8 characters long";
+        return false;
+    }
+
+    // Validates that repeat password is not empty
+    if(repeatPass.value === "") {
+        repeatPass.classList.add("uk-form-danger");
+        errorContainer.innerHTML = "Please repeat the password";
+        return false;
+    }
+
+    // Validates that password and repeated password matches
+    if (pass.value !== repeatPass.value) {
+        pass.classList.add("uk-form-danger");
+        repeatPass.classList.add("uk-form-danger");
+        errorContainer.innerHTML = "Passwords does not match";
+        return false;
+    }
+
+    // Submits form
+    document.forms['createAccountForm'].submit();
 }
---
+++ src/main/webapp/jsp/user/createUser.jsp
@@ -0,0 +1,79 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: madsbrandt
+  Date: 2019-10-18
+  Time: 20:55
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+	<title>Create User</title>
+	<link rel="stylesheet" href="${pageContext.request.contextPath}/css/style.css">
+	<link rel="stylesheet" href="${pageContext.request.contextPath}/css/uikit.min.css" />
+	<script src="${pageContext.request.contextPath}/js/script.js"></script>
+	<script src="${pageContext.request.contextPath}/js/uikit.min.js"></script>
+	<script src="${pageContext.request.contextPath}/js/uikit-icons.min.js"></script>
+	<script src="${pageContext.request.contextPath}/js/script.js"></script>
+</head>
+<body>
+	<%@include file="/jsp/nav.jsp"%>
+
+
+	<div class="uk-section uk-flex uk-animation-fade" uk-height-viewport>
+		<div class="uk-width-1-1">
+			<div class="uk-container">
+				<div class="uk-grid-margin uk-grid uk-grid-stack" uk-grid>
+					<div class="uk-width-1-1@m">
+						<div class="uk-margin uk-width-large uk-margin-auto uk-card uk-card-default uk-card-body uk-box-shadow-large">
+
+							<h3 class="uk-card-title uk-text-center">Create Account</h3>
+
+							<form name="createAccountForm" action="#" method="post" onsubmit="event.preventDefault(); validateAccountCreation();">
+								<input type="hidden" name="command" value="login">
+								<div class="uk-margin">
+									<div class="uk-inline uk-width-1-1">
+										<span class="uk-form-icon" uk-icon="icon: user"></span>
+										<input class="uk-input uk-form-large" id="name" type="text" name="userName" placeholder="Name*">
+									</div>
+								</div>
+								<div class="uk-margin">
+									<div class="uk-inline uk-width-1-1">
+										<span class="uk-form-icon" uk-icon="icon: mail"></span>
+										<input class="uk-input uk-form-large" id="email" type="text" name="text" placeholder="Email*">
+									</div>
+								</div>
+								<div class="uk-margin">
+									<div class="uk-inline uk-width-1-1">
+										<span class="uk-form-icon" uk-icon="icon: lock"></span>
+										<input class="uk-input uk-form-large" id="password" type="password" name="password" placeholder="Password*">
+									</div>
+								</div>
+								<div class="uk-margin">
+									<div class="uk-inline uk-width-1-1">
+										<span class="uk-form-icon" uk-icon="icon: lock"></span>
+										<input class="uk-input uk-form-large" id="repeatPassword" type="password" name="repeatPassword" placeholder="Repeat Password*">
+									</div>
+								</div>
+								<div class="uk-margin">
+									<button class="uk-button uk-button-primary uk-button-large uk-width-1-1">Create account</button>
+								</div>
+								<div class="uk-text-small uk-text-center">
+									<p id="errorMessage" style="color: #DC143C"></p>
+									<c:if test="${requestScope.error != null}">
+										<h4><c:out value="${requestScope.error}"/></h4>
+									</c:if>
+								</div>
+								<div class="uk-text-small uk-text-center uk-margin-small-top">
+									Already have an account? <a href="#">Log in</a>
+								</div>
+							</form>
+
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
