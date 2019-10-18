d40e310357604edeceea90269189ba0e2a2f7c7d
Benjamin Paepke <cph-bp133@cphbusiness.dk>
2019-10-18 10:18:27 +0200

    Work on UserMapper

---
+++ Tests/persistence/UserMapperTest.java
@@ -0,0 +1,21 @@
+package persistence;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import static org.junit.Assert.*;
+
+public class UserMapperTest {
+
+    @Before
+    public void setup() throws Exception {
+    }
+
+    @Test
+    public void test_getAllUsers() {
+    }
+}
