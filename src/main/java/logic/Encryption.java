ea261daf0ee356d612f79b586e2a782d1899b663
Tunoc <random959dude@gmail.com>
2019-10-24 11:11:33 +0200

    Encryption for passwords added

---
+++ src/main/java/logic/Encryption.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package logic;
+
+import java.math.BigInteger;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+/**
+ *
+ * @author rando
+ */
+public class Encryption {
+    public static String encryptPsw(String password) {
+        try {
+            byte[] messageDirect = getSHA(password);
+            return toHexString(messageDirect);
+        } catch (NoSuchAlgorithmException e) {
+            throw new SecurityException("Encryption Failed");
+        }
+    }
+
+    public static byte[] getSHA(String input) throws NoSuchAlgorithmException {
+        //Static getInstance methos is called with hashing SHA-512
+        /*
+        Message digests are secure one-way hash functions that take an input of
+        data and in return output a fixed-length hash value.
+        */
+        MessageDigest md = MessageDigest.getInstance("SHA-512");
+        /*
+        Digest() method is called to calculate the message digest of our string 
+        input, and return it as a array of byte
+        */
+        return md.digest(input.getBytes(StandardCharsets.UTF_8));
+    }
+
+    public static String toHexString(byte[] hash) {
+        //Convert byte array into signum representation(Negative -1, neutral 0, positive 1)
+        BigInteger number = new BigInteger(1, hash);
+        /*
+        Convert the big integer to Hex value by using the java.math.BigInteger.toString(int radix) method
+        When using a radix that is outside the range of Character.MIN_RADIX to Character.MAX_RADIX
+        then the radix is set to 10 as default.
+        When the Radix is 16 then the function will first convert BigInteger
+        to hexadecimal from then it will return the String representation of that hexadecimal number.
+        */
+        StringBuilder hexString = new StringBuilder(number.toString(16));
+        //Pad with leading zeroes
+        while (hexString.length() < 32) {
+            hexString.insert(0, '0');
+        }
+        return hexString.toString();
+    }
+}
